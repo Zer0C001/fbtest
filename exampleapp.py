9176afe73a31db74ba1f7d7e38319a661eeb38dd
Zer0C001 <georgi.saev@gmail.com>
2013-08-02 00:21:01 +0300

    test get suggestions

--- exampleapp.py
+++ exampleapp.py
@@ -188,8 +188,8 @@ def index():
         if num_cat==0:
         	init_cat=fb_call('app/objects/'+FBNS+':category',args={'access_token': app_access_token,'method':'POST', 'object': "{'title':'Uncategorized'}"})
         	#content+='   '+str(init_cat)
-        	l_obj=fb_call('app/objects/'+FBNS+':suggestion',args={'access_token': app_access_token,'fields':'id,created_time,pos_votes,neg_votes,category_id'})
-        	content=str(l_obj)+'x'
+        l_obj=fb_call('app/objects/'+FBNS+':suggestion',args={'access_token': app_access_token,'fields':'id,created_time,pos_votes,neg_votes,category_id'})
+        content=str(l_obj)
 
         return render_template(
             'index.html', app_id=FB_APP_ID, token=access_token, app=fb_app,
