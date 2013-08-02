e6e896951a058ae4254133dff8796771ee4e8a75
Zer0C001 <georgi.saev@gmail.com>
2013-08-02 23:51:52 +0300

    testing

--- exampleapp.py
+++ exampleapp.py
@@ -206,7 +206,7 @@ def index():
         #	suggestions=l_obj
         content=str(suggestions)+str(request.args)#+' '+str(request.form)+str(request.cookies)
         disp_suggestions=[]
-        for i in range(0,10):
+        for i in range(0,min(10,len(suggestions))):
 	  disp_suggestions+=fb_call(suggestions[i]['id'],args={'access_token': app_access_token})
         return render_template(
             'index.html', app_id=FB_APP_ID, token=access_token, app=fb_app,
