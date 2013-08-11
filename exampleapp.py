19e92b987531d5b1f579ddfc0bbb71d6d17831e3
Zer0C001 <georgi.saev@gmail.com>
2013-08-11 21:23:54 +0300

    testing

--- exampleapp.py
+++ exampleapp.py
@@ -41,7 +41,7 @@ def get_tokens(fbtiv=False,short_uat=False):
 		#
 		has_uat=False
 		if session.has_key('long_uat'):
-			  tmp_long_uat=cipher.decrypt(base64.urlsafe_b64decode(session['long_uat']))
+			  tmp_long_uat=base64.urlsafe_b64decode(cipher.decrypt(session['long_uat']))
 			  has_uat=True
 		if has_uat and (is_valid(app_access_token,tmp_long_uat)):
 			long_uat=tmp_long_uat
