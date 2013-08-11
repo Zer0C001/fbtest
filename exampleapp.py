3daa503b385265b4dc3a37c0cc8fa8281717cb10
Zer0C001 <georgi.saev@gmail.com>
2013-08-11 13:45:43 +0300

    testing

--- exampleapp.py
+++ exampleapp.py
@@ -270,7 +270,7 @@ def suggestion_new():
 		else:
 			fbc1='error saving'
 
-		dbg="save suggestion: <Br>"+content+"<br>"+str(fbc)+"<br>"+str(fbc1)+'<br>user: '+str(me)+'<br>perms:<br>'+str(perm)+'<br><br>'+str(request.form)
+		dbg="save suggestion: <Br>"+content+"<br>"+str(fbc)+"<br>"+str(fbc1)+'<br>user: '+str(me)+'<br>perms:<br>'+str(perm)+'<br><br>'+str(request.form)+str(access_token)
 		return render_template('suggestion_saved.html',me=me,dbg=dbg,content='')
 	
 @app.route('/suggestion/<int:suggestion_id>', methods=['GET', 'POST'])
